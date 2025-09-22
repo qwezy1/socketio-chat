93cee448a1c93ff2c9794b0d3713cebcbb06a524
qwezy1 <145523078+qwezy1@users.noreply.github.com>
2025-09-22 21:19:48 +1000

    upd

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// Setup basic express server
-const express = require('express');
+import {PrismaClient} from '@prisma/client';
+import express from 'express';
+import path from 'path';
+import {createServer} from 'http';
+import {Server} from 'socket.io';
+import {fileURLToPath} from 'url';
+
 const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+const port = process.env.PORT || 3000
+
+httpServer.listen(port, () => {
+    console.log('Server listening at port %d', port);
 });
-
+const client = new PrismaClient()
 // Routing
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Chatroom
@@ -18,59 +25,77 @@ app.use(express.static(path.join(__dirname, 'public')));
 let numUsers = 0;
 
 io.on('connection', (socket) => {
-  let addedUser = false;
+    let addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
+    socket.on('new message', async (data) => {
+        if (!socket.userId) return;
+
+        const savedMessage = await client.message.create({
+            data: {
+                text: data,
+                sender: { connect: { id: socket.userId } },
+            }
+        });
+
+        console.log(savedMessage);
+
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
     });
-  });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
+    socket.on('add user', async (username) => {
+        if (addedUser) return;
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+        let user = await client.user.findUnique({
+            where: {username}
+        });
+
+        if (!user) {
+            user = await client.user.create({
+                data: {username, password: ""}
+            });
+            console.log(user);
+        }
+
+        socket.userId = user.id;
+        socket.username = username;
+
+        ++numUsers;
+        addedUser = true;
+
+        const messages = await client.message.findMany({
+            orderBy: { created_at: 'asc' },
+            include: { sender: true }
+        });
+
+        socket.emit('load messages', messages);
+
+        socket.emit('login', { numUsers });
+        socket.broadcast.emit('user joined', { username: socket.username, numUsers });
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
     });
-  });
 
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
     });
-  });
 
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --numUsers;
+
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
     });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
 });
